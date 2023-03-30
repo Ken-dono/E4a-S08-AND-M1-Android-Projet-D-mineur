a9d9623e478493d011051193baa08998ec7c347c
lucaassito <81563248+lucaassito@users.noreply.github.com>
2023-03-30 13:07:29 +0200

    remake main_activity and classement crea

---
+++ app/src/main/java/com/example/e4a_s08_and_m1_android_projet_demineur/Classement.java
@@ -0,0 +1,14 @@
+package com.example.e4a_s08_and_m1_android_projet_demineur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Classement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classement);
+    }
+}
